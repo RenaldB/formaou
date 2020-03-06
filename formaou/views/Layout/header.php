6d1094885e883cb6cbc688d572e321f25090477e
zygof <46953622+zygof@users.noreply.github.com>
2020-03-06 14:02:46 +0400

    main.css

--- formaou/views/Layout/header.php
+++ formaou/views/Layout/header.php
@@ -8,6 +8,6 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="/dist/css/bootstrap.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="/dist/css/main.css">
   </head>
   <body>
